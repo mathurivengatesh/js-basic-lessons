569ac67fec1becf323105579af9bb64833ee156f
Mathuri V <math3025@global.chainsys.com>
2022-04-04 15:59:26 +0530

    included all files

--- src/angularjs/demob-app/src/app/app.component.spec.ts
+++ src/angularjs/demob-app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'democ-app'`, () => {
+  it(`should have as title 'demob-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('democ-app');
+    expect(app.title).toEqual('demob-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('democ-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('demob-app app is running!');
   });
 });
